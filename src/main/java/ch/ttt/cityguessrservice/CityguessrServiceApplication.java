f5d5885a16e6446744ffdbae88e46f2a0600cab5
thtsbe <thomas.tschachtli@zuehlke.com>
2021-09-13 13:04:57 +0200

    setup

---
+++ src/main/java/ch/ttt/cityguessrservice/CityguessrServiceApplication.java
@@ -0,0 +1,13 @@
+package ch.ttt.cityguessrservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityguessrServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityguessrServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/ttt/cityguessrservice/CityguessrServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.ttt.cityguessrservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityguessrServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
