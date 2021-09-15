986d0e31500c67bd2d3beb873dac48b48526dd49
thtsbe <thomas.tschachtli@zuehlke.com>
2021-09-15 10:59:48 +0200

    Id

--- src/main/java/ch/ttt/cityguessrservice/guess/Guess.java
+++ src/main/java/ch/ttt/cityguessrservice/guess/Guess.java
@@ -4,6 +4,7 @@ import ch.ttt.cityguessrservice.cities.City;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Guess {
+    @Id
     private UUID id;
     private City city;
     private UUID userId;
