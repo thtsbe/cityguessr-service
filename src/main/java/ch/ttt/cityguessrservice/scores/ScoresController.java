f0a97d750dc28fa730516e3c84b5846bc5153e3c
thtsbe <thomas.tschachtli@zuehlke.com>
2021-09-15 11:43:57 +0200

    empty guesses

--- src/main/java/ch/ttt/cityguessrservice/scores/ScoresController.java
+++ src/main/java/ch/ttt/cityguessrservice/scores/ScoresController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -20,7 +21,7 @@ import java.util.stream.StreamSupport;
 @RestController
 @RequestMapping("/scores")
 @RequiredArgsConstructor
-public class ScoreController {
+public class ScoresController {
     private final GuessRepository guessRepository;
     private final GuessAnswerRepository answerRepository;
     private final CheckinRepository checkinRepository;
@@ -31,27 +32,32 @@ public class ScoreController {
     }
 
     private List<Scores> scoresByUser() {
-        final Map<UUID, List<Guess>> map = StreamSupport.stream(guessRepository.findAll().spliterator(), false)
-                .filter(g -> g.getUserId() != null)
-                .collect(Collectors.groupingBy(Guess::getUserId));
-
-
-        return map.entrySet().stream()
+        return getGuessesByUser().entrySet().stream()
                 .map(e -> createScore(e.getKey(), e.getValue()))
                 .collect(Collectors.toList());
     }
 
-    private Scores createScore(final UUID userId, final List<Guess> guesses) {
-        final List<Score> scores = guesses.stream()
-                .map(this::mapToScore)
-                .collect(Collectors.toList());
+    private Map<UUID, List<Guess>> getGuessesByUser() {
+        return StreamSupport.stream(guessRepository.findAll().spliterator(), false)
+                .filter(g -> g.getUserId() != null)
+                .collect(Collectors.groupingBy(Guess::getUserId));
+    }
 
+    private Scores createScore(final UUID userId, final List<Guess> guesses) {
+        final List<Score> scores = createScores(guesses);
         return new Scores(
                 getUsername(userId),
-                guesses.size(),
+                scores.size(),
                 calculatePoints(scores),
                 scores
         );
+    }
+
+    private List<Score> createScores(final List<Guess> guesses) {
+        return guesses.stream()
+                .map(this::mapToScore)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
     }
 
     private String getUsername(final UUID userId) {
@@ -86,7 +92,9 @@ public class ScoreController {
 
     private Score mapToScore(final Guess guess) {
         final List<GuessAnswer> answers = answerRepository.findAllByGuessId(guess.getId());
-
+        if (answers.isEmpty()){
+            return null;
+        }
         return new Score(
                 guess.getCity().getCity(),
                 answers.stream().anyMatch(a -> guess.getCity().getId().equals(a.getCityId())),
